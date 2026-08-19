7a160752a04283d72b47f1f05adecc3a263d4529
Wamiq Haider <swamiqhz234@gmail.com>
2026-08-19 16:20:16 +0500

    version 2

--- berealclone/src/app/(tabs)/index.tsx
+++ berealclone/src/app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>stuff.</Text>
-      <TextInput placeholder="Email"/>
+      <TextInput placeholder="Email is happening here"/>
       <ActivityIndicator size={"large"}/>
       <Link href={"/about"}>Go to about screen</Link>
       <Host>
